dcec782af98b5f3310d83ee17a9338b174502f36
Wayne Bishop <waynewbishop@users.noreply.github.com>
2026-03-18 18:51:51 -0700

    Initial Commit

---
+++ bishop-quiver-test-watchos Watch App/bishop_quiver_test_watchosApp.swift
@@ -0,0 +1,17 @@
+//
+//  bishop_quiver_test_watchosApp.swift
+//  bishop-quiver-test-watchos Watch App
+//
+//  Created by Wayne Bishop on 3/18/26.
+//
+
+import SwiftUI
+
+@main
+struct bishop_quiver_test_watchos_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
